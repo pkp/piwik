bf98de6fcd09da636414ebba222b515ca72b98bd
Alec Smecher <alec@smecher.bc.ca>
2019-03-04 14:29:24 -0800

    Update copyright dates

--- index.php
+++ index.php
@@ -7,8 +7,8 @@
 /**
  * @file plugins/generic/piwik/index.php
  *
- * Copyright (c) 2013-2018 Simon Fraser University
- * Copyright (c) 2003-2018 John Willinsky
+ * Copyright (c) 2013-2019 Simon Fraser University
+ * Copyright (c) 2003-2019 John Willinsky
  * Distributed under the GNU GPL v2. For full terms see the file docs/COPYING.
  *
  * @ingroup plugins_generic_piwik
@@ -20,4 +20,3 @@ require_once('PiwikPlugin.inc.php');
 
 return new PiwikPlugin();
 
-?>
